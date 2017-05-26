91bf626f81ffba35b7ef0def124328b505648c1b
Paul Schifferer <paul@schifferers.net>
2017-05-26 14:26:41 -0700

    Added mentions, interactions, and pins to the Blurbs resource.

--- TenCenturiesKit/Requests/Blurbs.swift
+++ TenCenturiesKit/Requests/Blurbs.swift
@@ -3,27 +3,70 @@ import Foundation
 
 public struct Blurbs {
 
-    /// Retrieves the home timeline.
-    ///
-    /// - Parameter range: The bounds used when requesting data from Pnut.io.
-    /// - Returns: Request for `[Post]`.
+    /**
+     Retrieves the home timeline.
+
+     - parameter range: The bounds used when requesting data from 10 Centuries.
+     - returns: Request for `[Post]`.
+     */
     public static func home(range : RequestRange = .default) -> Request<[Post]> {
         let rangeParameters = range.parameters(limit: between(1, and: 40, fallback: 20))
-//        let localParameters = r
+        //        let localParameters = r
         let method = HTTPMethod.get(Payload.parameters(rangeParameters))
 
         return Request<[Post]>(path: "/content/blurbs/home", method: method, parse: Request<[Post]>.parser)
     }
 
-    /// Retrieves the global timeline.
-    ///
-    /// - Parameter range: The bounds used when requesting data from Pnut.io.
-    /// - Returns: Request for `[Post]`.
+    /**
+     Retrieves the global timeline.
+
+     - parameter range: The bounds used when requesting data from 10 Centuries.
+     - returns: Request for `[Post]`.
+     */
     public static func global(range : RequestRange = .default) -> Request<[Post]> {
         let rangeParameters = range.parameters(limit: between(1, and: 40, fallback: 20))
         let method = HTTPMethod.get(Payload.parameters(rangeParameters))
 
         return Request<[Post]>(path: "/content/blurbs/global", method: method, parse: Request<[Post]>.parser)
+    }
+
+    /**
+     Retrieves interactions for the current account.
+
+     - parameter range: The bounds used when requesting data from 10 Centuries.
+     - returns: Request for `[Post]`.
+     */
+    public static func interactions(range : RequestRange = .default) -> Request<[Post]> {
+        let rangeParameters = range.parameters(limit: between(1, and: 40, fallback: 20))
+        let method = HTTPMethod.get(Payload.parameters(rangeParameters))
+
+        return Request<[Post]>(path: "/content/blurbs/interactions", method: method, parse: Request<[Post]>.parser)
+    }
+
+    /**
+     Retrieves mentions for the current account.
+
+     - parameter range: The bounds used when requesting data from 10 Centuries.
+     - returns: Request for `[Post]`.
+     */
+    public static func mentions(range : RequestRange = .default) -> Request<[Post]> {
+        let rangeParameters = range.parameters(limit: between(1, and: 40, fallback: 20))
+        let method = HTTPMethod.get(Payload.parameters(rangeParameters))
+
+        return Request<[Post]>(path: "/content/blurbs/mentions", method: method, parse: Request<[Post]>.parser)
+    }
+
+    /**
+     Retrieves pins for the current account.
+
+     - parameter range: The bounds used when requesting data from 10 Centuries.
+     - returns: Request for `[Post]`.
+     */
+    public static func pins(range : RequestRange = .default) -> Request<[Post]> {
+        let rangeParameters = range.parameters(limit: between(1, and: 40, fallback: 20))
+        let method = HTTPMethod.get(Payload.parameters(rangeParameters))
+
+        return Request<[Post]>(path: "/content/blurbs/pins", method: method, parse: Request<[Post]>.parser)
     }
     
 }
