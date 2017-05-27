3f1defc4f02299821843ed59a382d9f56d7f49f7
Paul Schifferer <paul@schifferers.net>
2017-05-27 07:39:53 -0700

    Added accounts resource.

---
+++ TenCenturiesKit/Requests/Accounts.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+
+public struct Accounts {
+
+    public static func account(id : Int) -> Request<Account> {
+        let method = HTTPMethod.get(Payload.empty)
+
+        return Request<Account>(path: "/users/\(id)", method: method, parse: Request<Account>.parser)
+    }
+
+//    public static func blurbs(id : Int) -> Request<Account> {
+//        let method = HTTPMethod.get(Payload.empty)
+//
+//        return Request<Account>(path: "/users/blurbs/\(id)", method: method, parse: Request<Account>.parser)
+//    }
+
+}
